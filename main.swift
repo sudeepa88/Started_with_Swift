8da1ab9ac1085ff17fe7777c32645ea1f13f7540
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-08-11 15:02:55 +0530

    Swift

---
+++ main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  practice
+//
+//  Created by sudeepa pal on 10/08/23.
+//
+
+
+
+//print("Hello, World!")
+var num=10;
+var someText:String
+someText="How are You?"
+print(someText,num)
+
+let a1:String="Ram is eating"
+print(a1)
+
+//using terminator
+print("Good morning",terminator: ",")
+print("How are you ?")
